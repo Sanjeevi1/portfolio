50f61cbfb63231074d302d851b2c3935eedbbd69
Sanjeevi <sanjeevi2210539@ssn.edu.in>
2024-08-02 21:11:53 +0530

    first commit

---
+++ portfolio-main/data/index.ts
@@ -0,0 +1,146 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Turf Booking System",
+    description: "Book your game, own the field ",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "VEGETABLE VENDOR SYSTEM",
+    des: "Enabled customers to order fresh vegetables with customized quantities and real- time updates.",
+    img: "/vege.jpg",
+    iconLists: ["/django.svg", "/icons8-js.svg", "/icons8-html.svg", "/icons8-css.svg", "/icons8-sql-64.png"],
+    link: "https://www.drive.google.com",
+  },
+  {
+    id: 2,
+    title: "Internship Management System",
+    des: "Created a centralized digital platform for internship management..",
+    img: "/internship.png",
+    iconLists: ["/django.svg", "/icons8-js.svg", "/icons8-html.svg", "/icons8-css.svg", "/icons8-sql-64.png"],
+    link: "https://www.drive.google.com",
+  },
+  {
+    id: 3,
+    title: "Turf Booking System",
+    des: "Our turf booking website allows users to quickly and easily reserve sports fields for their favorite activities. Whether you're planning a soccer match, cricket game, or any other sport, our platform lets you browse available slots, compare prices, and book your turf instantly—giving you more time to focus on the game!",
+    img: "/turf.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/icons8-html.svg", "/icons8-sql-64.png"],
+    link: "https://www.drive.google.com",
+  },
+
+];
+
+export const testimonials = [
+  {
+    /*quote: "I am currently pursuing my BTech in Information Technology at SSN College of Engineering, where I am actively exploring full stack development. My academic journey has equipped me with a solid foundation in both front-end and back-end technologies, and I am eager to expand my skills further. I am passionate about building seamless, user-friendly applications and continuously seek out opportunities to deepen my knowledge in areas such as web development, database management, and software architecture. My goal is to contribute to innovative projects that bridge the gap between cutting-edge technology and user experience.",
+    */
+    id: 1,
+    quote: "I am a BTech Information Technology student at SSN College of Engineering, focusing on full stack development. I am passionate about creating seamless, user-friendly applications and continually enhancing my skills in front-end and back-end technologies. My goal is to contribute to innovative projects that integrate technology with excellent user experience.",
+    name: "SSN COLLEGE OF ENGINEERING",
+    title: "Btech Information Technology CGPA:8.41",
+  },
+  {
+    id: 2,
+    quote: "I completed my 12th studies with a remarkable 99.6% score, which reflects my dedication and strong academic background. This achievement has laid a solid foundation for my ongoing pursuit of excellence in Information Technology at SSN College of Engineering.",
+    name: "Senthil Matric Hr Sec School",
+    title: "HSC 579/600",
+  }
+];
+
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Prodigy InfoTech",
+    subtitle: "Full Stack Developer Intern",
+    desc: "I am currently working as a Web Developer at Prodigy Infotech in Chennai, Tamil Nadu, where I develop and maintain responsive, user-friendly web applications using HTML, CSS, JavaScript, and modern front-end frameworks like React, Angular, and Vue.js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "ACADEMIC PROJECTS ",
+    subtitle: "Team Lead",
+    desc: "I led teams for academic projects like Vegetable Vendor, Internship Management System and etc.., where I contributed to coding and coordinated project execution using various technologies and tools, ensuring timely delivery and high-quality outcomes.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/"
+  },
+  {
+    id: 2,
+    img: "/leetcode.svg",
+    link: "https://leetcode.com/u/sanjeevi2210539/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: 'https://www.linkedin.com/in/sanjeevi-n-038b26259',
+  },
+];
